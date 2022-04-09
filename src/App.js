d3d882a2c0e0cb08b504f9bd67f2aeaf6f655ff4
Anshika Gusain <anshikagusain62@gmail.com>
2022-04-09 22:31:42 +0530

    Removing About page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
 import {useState} from "react";
-import About from './components/About';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  BrowserRouter
-} from "react-router-dom";
+// import About from './components/About';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link,
+//   BrowserRouter
+// } from "react-router-dom";
 function App() {
   const [mode,setMode]=useState('light');
   const changeMode=()=>{
@@ -27,13 +27,14 @@ function App() {
   }
   return (
     <>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <Navbar changeMode={changeMode} mode={mode}/>
-      <Routes>
+      {/* <Routes>
           <Route exact path="/about" element={<About mode={mode} />}/>
           <Route exact path="/" element={<Textarea mode={mode}/>}/>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
+      <Textarea mode={mode}/>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,16 @@ const Navbar=({changeMode,mode})=>{
         <>
             <nav className={`navbar navbar-expand-lg navbar-${mode==="light"?"light":"dark"} bg-${mode==="light"?"light":"dark"}`}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"><strong>Text Manipulator</strong></Link>
+                    <a className="navbar-brand" href="/"><strong>Text Manipulator</strong></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
-                            <Link className="nav-link" aria-current="page" to="/about">About</Link>
+                            {/* <a className="nav-link" aria-current="page" href="/about">About</a> */}
                         </ul>
                         <div className="form-check form-switch">
                             <input onChange={changeMode} className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
