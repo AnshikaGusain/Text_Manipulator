0989b579562612ef8144c121047b0eb1c5b38306
Anshika Gusain <anshikagusain62@gmail.com>
2022-04-24 12:16:04 +0530

    Adding more functionality

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -28,10 +28,15 @@ const Textarea=({mode})=>{
                 <textarea onChange={onChange} style={{backgroundColor:`${mode==="light"?"white":"#213549"}`,color:`${mode==="light"?"black":"white"}`}} value={text} className="form-control" id="textarea" rows="15"></textarea>
             </div>
             <div className='container'>
-                <button onClick={onChangeLower} type="button" className={`btn btn-outline-${mode==="light"?"dark":"light"} mx-1`}>Lowercase</button>
-                <button onClick={onChangeUpper} type="button" className={`btn btn-outline-${mode==="light"?"dark":"light"} mx-1`}>Uppercase</button>
-                <button onClick={onClear} type="button" className={`btn btn-outline-${mode==="light"?"dark":"light"} mx-1`}>Clear Text</button>
-                <button onClick={onCopy} type="button" className={`btn btn-outline-${mode==="light"?"dark":"light"} mx-1`}>Copy Text</button>
+                <button disabled={text.length===0} onClick={onChangeLower} type="button" className={`btn btn-outline-${mode==="light"?"dark":"light"} mx-1 my-1`}>Lowercase</button>
+                <button disabled={text.length===0} onClick={onChangeUpper} type="button" className={`btn btn-outline-${mode==="light"?"dark":"light"} mx-1 my-1`}>Uppercase</button>
+                <button disabled={text.length===0} onClick={onClear} type="button" className={`btn btn-outline-${mode==="light"?"dark":"light"} mx-1 my-1`}>Clear Text</button>
+                <button disabled={text.length===0} onClick={onCopy} type="button" className={`btn btn-outline-${mode==="light"?"dark":"light"} mx-1 my-1`}>Copy Text</button>
+            </div>
+            <div className='container'>
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
             </div>
         </>
     )
